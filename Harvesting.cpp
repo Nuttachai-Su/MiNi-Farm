7c1d159c764ca104d2c728ea5a4b3807d69ca3f9
ChoppyChoppa <nonthananhinmalai@gmail.com>
2026-03-10 13:51:48 +0700

    add comment

--- Harvesting.cpp
+++ Harvesting.cpp
@@ -6,12 +6,12 @@ void harvesting() {
     int r; cout << "Row: "; cin >> r; r--; //รับค่าตำแหน่งแถว
     if (r >= 0 && r < (int)rows.size() && rows[r].isPlanted) {
         int ch = baseSuccess[rows[r].plantTypeIndex] + (rows[r].isWatered ? 20 : 0) + (rows[r].hasFertilizer ? 15 : 0); //คำนวณเปอร์เซ็นการเก็บเกี่ยวสำเร็จ
-        if (rows[r].age < 2) ch -= 40;
-        if (!rows[r].isRotten && (rand() % 100) < ch) {
-            int s = plantSells[rows[r].plantTypeIndex] + (rows[r].age * 5); M += s;
+        if (rows[r].age < 2) ch -= 40; //เช็คอายุพืช ถ้าอายุน้อยกว่า 2 โอกาสจะลดลง 40
+        if (!rows[r].isRotten && (rand() % 100) < ch) { //เช็คถ้าพืชไม่เน่าและสุ่มเปอร์เซ็นได้น้อยกว่าโอกาสเก็บเกี่ยวสำเร็จ
+            int s = plantSells[rows[r].plantTypeIndex] + (rows[r].age * 5); M += s; //ราคาขายคำนวณจากเงินต้นของพืช + อายุของพืชคูณ 5
             cout << "[Success] Sold for $" << s;
         } else cout << "[Fail] Crop lost!";
-        rows[r].isPlanted = false; rows[r].hasFertilizer = false; AP--; waitEnter();
+        rows[r].isPlanted = false; rows[r].hasFertilizer = false; AP--; waitEnter(); //ตั้งค่าแปลงปลูกและปุ๋ยเป็น false ลบค่า AP และรอ Player กด enter 
     }
 }
 
